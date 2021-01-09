f91192e0a2cd251cefa0c8813657d242e1da26d8
vladi2703 <46690885+vladi2703@users.noreply.github.com>
2021-01-09 22:35:08 +0200

    HW3 snow and tasks

---
+++ LetItSnow.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>     
+#include <time.h>
+#include <thread>        
+
+void snow(int, int, int);
+void print(char**, int, int);
+void wait(int);
+void clear();
+void moveDown(char**, int); 
+int main()
+{
+    srand(time(NULL));
+    snow(10, 20, 10);
+} 
+void clear()
+{
+    system("cls");
+}
+void wait(int seconds)
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(seconds * 1000));
+}
+void print(char** matrix, int height, int width)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            std::cout << matrix[i][j];
+        }
+        std::cout << '\n';
+    }
+}
+void moveDown(char** matrix, int height)
+{
+    for (int i = height-1; i > 0 ; i--)
+    {
+        for (int j = 0; j < 20; j++)
+        {
+            matrix[i][j] = matrix[i - 1][j];
+        }
+    }
+
+}
+void snow(int seconds, int width, int height)
+{
+    char** picture = new char* [height];
+    for (int i = 0; i < height; i++)
+    {
+        picture[i] = new char[width+1];
+            for (int j = 0; j < width; j++)
+            {
+                picture[i][j] = ' ';
+            }
+            picture[i][width] = '\0';
+    }
+
+    while(true)
+    {
+        moveDown(picture, height);
+        for (int k = 0; k < width; k++)
+        {
+            bool snowflake = rand() % 2; //random function - result 0 or 1
+            if (snowflake)
+            {
+                picture[0][k] = '*';
+            }
+            else
+            {
+                picture[0][k] = ' ';
+            }
+        }
+       
+        wait(1);
+        clear();
+        print(picture, height, width);
+    } 
+} 
+
+// TODO: Snow @ the bottom
+//Less snowflakes
+
---
+++ TasksSession.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+void printMatrix(double** matrix, int height, int width)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            std::cout << matrix[i][j] << ' ';
+        }
+        std::cout << '\n';
+    }
+}
+void printCompleteTasks(double** matrix, int size)
+{
+    std::cout << "Tasks: ";
+    for (int i = size; i >= 0; i--)
+    {
+        std::cout << matrix[i][0] << ' ';
+    }
+    std::cout << '\n';
+}
+void sortTasks(double** taskChar, int taskCount)
+{   
+    
+    for (int i = 0; i < taskCount; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < taskCount; j++)
+        {
+            if (taskChar[j][3] < taskChar[minIndex][3])
+            {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i)
+        {
+            std::swap(taskChar[i], taskChar[minIndex]);
+        }
+    }
+
+
+
+}
+int distributeTasks(double** taskChar, int totalTime, int* counter, int tasksCount)
+{
+    if (*counter >= tasksCount || totalTime < int(taskChar[*counter][1]))
+    {
+        return totalTime;
+    }
+    totalTime -= int(taskChar[*counter][1]);
+    *counter += 1;
+    distributeTasks(taskChar, totalTime, counter, tasksCount);
+}
+void weightCalculation(double** taskChar, int* minutesPerDay, int daysLeft, int tasksCount, int* totalTime)
+{
+    *totalTime = 0;
+    for (int i = 0; i < daysLeft; i++) // calculate total time
+    {
+        *totalTime += minutesPerDay[i];
+    }
+    for (int i = 0; i < tasksCount; i++)    ///calculate weight of every task
+    {
+        taskChar[i][3] = taskChar[i][2] / taskChar[i][1];
+    }
+}
+int main()
+{
+    int tasksCount, daysLeft;
+    std::cin >> tasksCount;
+    double** taskChar = new double*[tasksCount];
+    for (int i = 0; i < tasksCount; i++)
+    {
+        taskChar[i] = new double[4];
+        for (int j = 0; j < 3; j++)
+        {
+            std::cin >> taskChar[i][j];
+        }
+    }
+    std::cin >> daysLeft;
+    int* minutesPerDay = new int[daysLeft];
+    for (int i = 0; i < daysLeft; i++)
+    {
+        std::cin >> minutesPerDay[i];
+    }
+
+    int totalTime, timeRemaining;
+    int tasksComplete = 0;
+    
+    weightCalculation(taskChar, minutesPerDay, daysLeft, tasksCount, &totalTime);
+    sortTasks(taskChar, tasksCount);
+    timeRemaining = distributeTasks(taskChar, totalTime, &tasksComplete, tasksCount);
+    tasksComplete--; //recursion adds 1 before return
+    int minutes = timeRemaining % 60; 
+    int hours = timeRemaining/60; 
+    printCompleteTasks(taskChar, tasksComplete);
+    std::cout << "Time remaining " << hours << ':' << minutes << std::endl;
+
+    //std::cout << distributeTasks(taskChar, totalTime, 0, tasksCount) << std::endl;
+   printMatrix(taskChar, tasksCount, 4);
+}
+
+//TODO: AKO NE SA PODREDENI PO VREME - PURVI PRIMER
